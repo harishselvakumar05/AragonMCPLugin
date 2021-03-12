c39453c1db029d076a21587dc818122d690a55e5
harish <73505107+harishlikespi@users.noreply.github.com>
2021-03-11 21:48:50 -0800

    Update main.java

--- AragonMCServerPlugin/src/com/test/selvakumar/main.java
+++ AragonMCServerPlugin/src/com/test/selvakumar/main.java
@@ -4,11 +4,11 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-
+//TEST
 
 public class main extends JavaPlugin {
 
-	
+
 	@Override
 	public void onEnable() {  
 		getServer().getPluginManager().registerEvents(new MatchCommands(this), this);
@@ -17,13 +17,15 @@ public class main extends JavaPlugin {
 			for(Player player : Bukkit.getOnlinePlayers()) {
 				player.getInventory().clear();
 				//	player.setGameMode(GameMode.ADVENTURE);
+				
+				BoardPlayer boardplayer = new BoardPlayer(player, 0);
+				MatchCommands.LeaderBoardPlayers.add(boardplayer);
 				MatchCommands.createBoard(player);
 			}
 		}
 		getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "enabled"); 
 	}
 
-    
 
  
     @Override
